6b26e9d4ba0a1e4f8b513aaa961b1017c03dabea
Abhishek <abhishek.iemmca12@gmail.com>
2016-09-16 16:40:59 +0530

    Initial commit with gitignore file added

---
+++ index.js.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var users = [ { name : "Anil "},{ name : "Xyz "} ]
+
+function readFile(  name , cb ){
+fs.readFile( "files/" +name + ".txt" , function (err, data) {
+   if (err) {
+       return cb(err);
+   }
+   cb( err , data.toString());
+});
+}
+
+app.get( "/users" , function( req , res , next ){
+    var token = req.query.token;
+    if(!token){
+        res.send("Erroir")
+    }else{
+        next()
+    }
+},
+ function(req , res ){
+res.send( users );
+})
+
+app.all('/learn',function(req,res){
+    var technology = req.query.technology;
+    readFile( technology, function( err , data){
+    res.send(data);
+    })
+});
+
+
+app.listen(3000,function(){
+    console.log('App is listening on port 3000');
+});
